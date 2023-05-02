8ee6336ddb06d7b5d65da754085f00d1c5b60904
Andrianarivo David <andrianarivo.david@gmail.com>
2023-05-02 18:56:44 +0300

    finish edit task

--- src/modules/editTask.js
+++ src/modules/editTask.js
@@ -1,15 +1,5 @@
 import listOfTasks from './listOfTasks.js';
 
-const createInput = (taskTitle) => {
-  const input = document.createElement('input');
-  input.style.border = 'none';
-  input.style.outline = 'none';
-  input.style.fontStyle = 'italic';
-  input.style.backgroundColor = 'bisque';
-  input.value = taskTitle;
-  return input;
-};
-
 const updateTask = (index, input, taskStore) => {
   input.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
@@ -19,29 +9,52 @@ const updateTask = (index, input, taskStore) => {
   });
 };
 
+const createTrashCan = (taskIndex) => {
+  const a = document.createElement('a');
+  a.href = '#';
+  a.style.textDecoration = 'none';
+  const i = document.createElement('i');
+  i.className = 'delete-task fa-regular fa-trash-can';
+  i.id = taskIndex;
+  a.appendChild(i);
+  return a;
+};
+
+const createInputEdit = (target) => {
+  // INPUT EDIT TEXT
+  const taskTitle = target.children[0].children[1].innerHTML;
+  const input = document.createElement('input');
+  input.style.border = 'none';
+  input.style.outline = 'none';
+  input.style.fontStyle = 'italic';
+  input.style.backgroundColor = 'bisque';
+  input.value = taskTitle;
+
+  // LAYOUT CHANGE
+  target.style.backgroundColor = 'bisque';
+  target.children[0].children[1].remove();
+  target.children[0].appendChild(input);
+  target.children[1].remove();
+
+  // TRASH CAN
+  const deleteTask = createTrashCan(target.id);
+  target.appendChild(deleteTask);
+
+  input.focus();
+
+  return input;
+};
+
 const editTask = (taskStore) => {
   document.addEventListener('click', (event) => {
     const { target } = event;
     if (target.classList.contains('task-item')) {
-      const taskTitle = target.children[0].children[1].innerHTML;
-      const input = createInput(taskTitle);
-
-      target.style.backgroundColor = 'bisque';
-      target.children[0].children[1].remove();
-      target.children[0].appendChild(input);
-      input.focus();
-
+      const input = createInputEdit(target);
       updateTask(target.id, input, taskStore);
     } else if (target.classList.contains('task-title')) {
-      const taskTitle = target.innerHTML;
-      const input = createInput(taskTitle);
       const parent = target.parentNode.parentNode;
 
-      parent.style.backgroundColor = 'bisque';
-      parent.children[0].children[1].remove();
-      parent.children[0].appendChild(input);
-      input.focus();
-
+      const input = createInputEdit(parent);
       updateTask(target.id, input, taskStore);
     }
   });
